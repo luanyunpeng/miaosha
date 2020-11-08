3acad7bf7c9ae0164e851202a142bf6d3ed515c6
luanyunpeng <171250626@smail.nju.edu.cn>
2020-11-08 23:31:31 +0800

    1

---
+++ src/main/java/cn/hfbin/seckill/dao/UserMapper.java
@@ -0,0 +1,12 @@
+package cn.hfbin.seckill.dao;
+
+import cn.hfbin.seckill.entity.User;
+import org.apache.ibatis.annotations.Param;
+
+
+public interface UserMapper {
+
+    User selectByPhoneAndPassword(@Param("phone") String phone , @Param("password") String password);
+
+    User checkPhone(@Param("phone") String phone );
+}
